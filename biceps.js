532a044b78bb7a770a6e4e712fcbb6eba7c668fc
Balaji61 <balajik8555@gmail.com>
2025-12-23 18:37:51 +0530

    Responsive fix + Right arm tracking fix

--- biceps.js
+++ biceps.js
@@ -1,25 +1,48 @@
-let bicepsCount = 0;
-let bStage = null;
+let leftCount = 0;
+let rightCount = 0;
 
-function angle(a,b,c){
-  let radians = Math.atan2(c.y-b.y, c.x-b.x) -
-                Math.atan2(a.y-b.y, a.x-b.x);
-  let deg = Math.abs(radians*180/Math.PI);
-  if(deg>180) deg = 360-deg;
+let leftStage = "down";
+let rightStage = "down";
+
+function angle(a, b, c) {
+  let radians =
+    Math.atan2(c.y - b.y, c.x - b.x) -
+    Math.atan2(a.y - b.y, a.x - b.x);
+
+  let deg = Math.abs((radians * 180) / Math.PI);
+  if (deg > 180) deg = 360 - deg;
   return deg;
 }
 
-function handleBiceps(lm){
-  let shoulder = lm[11];
-  let elbow = lm[13];
-  let wrist = lm[15];
+function handleBiceps(lm) {
+  // LEFT ARM
+  let lShoulder = lm[11];
+  let lElbow = lm[13];
+  let lWrist = lm[15];
 
-  let a = angle(shoulder, elbow, wrist);
+  let leftAngle = angle(lShoulder, lElbow, lWrist);
 
-  if(a > 160) bStage = "down";
-  if(a < 40 && bStage === "down"){
-    bStage = "up";
-    bicepsCount++;
-    countText.innerText = "Reps: " + bicepsCount;
+  if (leftAngle > 150) leftStage = "down";
+
+  if (leftAngle < 55 && leftStage === "down") {
+    leftStage = "up";
+    leftCount++;
   }
+
+  // RIGHT ARM
+  let rShoulder = lm[12];
+  let rElbow = lm[14];
+  let rWrist = lm[16];
+
+  let rightAngle = angle(rShoulder, rElbow, rWrist);
+
+  if (rightAngle > 150) rightStage = "down";
+
+  if (rightAngle < 55 && rightStage === "down") {
+    rightStage = "up";
+    rightCount++;
+  }
+
+  countText.innerText =
+    `Left: ${leftCount} | Right: ${rightCount} | Total: ${leftCount + rightCount}`;
 }
